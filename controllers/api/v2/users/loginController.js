be1f948ad8298cb7de44d62fb48e34033fea2724
estefanoquiriconi <estefanoquiriconi@gmail.com>
2024-10-11 18:27:15 -0300

    refactor: loginController

--- controllers/api/v2/users/loginController.js
+++ controllers/api/v2/users/loginController.js
@@ -1,6 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs')
-const userSerive = require('../../../../services/user/index.service')
+const userService = require('../../../../services/user/index.service')
 const { notAuthorizedError, notFoundError, forbiddenError, badRequestError } = require('../../../../helpers/errors.helper')
 const { createToken } = require('../../../../services/security/createToken.service')
 
@@ -12,22 +12,19 @@ const login = async (req, res, next) => {
         }
         const { email, password } = req.body
 
-        const user = await userSerive.getByEmail(email)
+        const user = await userService.getByEmail(email)
         if (!user) {
             notFoundError('Usuario no encontrado', 'USER_NOT_FOUND')
         }
 
-        const passwordOk = bcryptjs.compareSync(password, user.password);
+        const passwordOk = await bcryptjs.compare(password, user.password);
         if (!passwordOk) {
             notAuthorizedError('Credenciales incorrectas', 'INVALID_CREDENTIALS')
         }
 
-        if (!user.active && user.registrationCode != null) {
-            forbiddenError('El usuario aún no fue activado', 'PENDING_ACTIVATION')
-        }
-
-        if (!user.active && user.registrationCode == null) {
-            forbiddenError('El usuario está desactivado', 'USER_INACTIVE')
+        if (!user?.active) {
+            const errorMessage = user?.registrationCode ? 'El usuario aún no fue activado' : 'El usuario está desactivado';
+            forbiddenError(errorMessage, user?.registrationCode ? 'PENDING_ACTIVATION' : 'USER_INACTIVE');
         }
 
         const tokenInfo = {
